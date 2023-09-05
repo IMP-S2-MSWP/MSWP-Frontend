58de30b3fcb5f5e36ceeec5746f6126e890b7f84
lsy000428 <lsy000428@naver.com>
2023-09-05 22:51:05 +0900

    MOB-12 회원가입 사용자입력 구현

--- MyApp/components/RegisterForm.tsx
+++ MyApp/components/RegisterForm.tsx
@@ -1,19 +1,46 @@
 import React, { useState } from 'react';
 import { Button, TextInput } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 interface Props {
-  onRegister: (username: string, password: string) => void;
+  onRegister: (id:string,password:string,username:string,nickname:string,sex:string,birthDate:String) => void;
 }
 
 const RegisterForm: React.FC<Props> = ({ onRegister }) => {
-  const [username, setUsername] = useState('');
+  const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [sex, setSex] = useState('');
+  const [birthDateYear,setBirthDateYear]=useState("2000");
+  const [birthDate,setBirthDate]=useState('');
 
   return (
     <>
-      <TextInput placeholder="Username" onChangeText={setUsername} />
+      <TextInput placeholder="ID" onChangeText={setId} />
       <TextInput placeholder="Password" secureTextEntry onChangeText={setPassword} />
-      <Button title="Register" onPress={() => onRegister(username,password)} />
+      <TextInput placeholder="Username" onChangeText={setUsername} />
+      <TextInput placeholder="Nickname" onChangeText={setNickname} />
+
+      {/* 성별 선택 */}
+      <Picker selectedValue={sex} onValueChange={(itemValue) => setSex(itemValue)}>
+        <Picker.Item label="Male" value="male" />
+        <Picker.Item label="Female" value="female" />
+        {/* 필요한 경우 다른 옵션 추가 가능 */}
+      </Picker>
+      {/* 생년 선택 */}
+     <Picker selectedValue={birthDateYear} onValueChange={(itemValue)=>setBirthDateYear(itemValue)}>
+       {[...Array(100)].map((_,i)=>(
+         //1900년부터 현재까지의 연도를 표시합니다.
+         //필요에 따라 범위를 조정할 수 있습니다.
+         <Picker.Item key={`year-${1900+i}`} label={`${1900+i}`} value={`${1900+i}`}/>
+       ))}
+     </Picker>
+
+     {/* 월/일은 텍스트 인풋으로 받는다고 가정하고 코드 작성 */}
+     {/* 이 부분은 필요에 따라 본인의 앱에 맞게 수정하세요. */}
+     <TextInput placeholder="MM-DD" onChangeText={setBirthDate} />
+     <Button title="Register" onPress={() => onRegister(id,password,username,nickname,sex,birthDateYear+birthDate)} />
     </>
   );
 };
--- MyApp/screens/RegisterScreen.tsx
+++ MyApp/screens/RegisterScreen.tsx
@@ -6,19 +6,19 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import RegisterForm from '../components/RegisterForm';
 import { RootStackParamList } from '../types';
 
-type RegisterScreenNavigationProp = StackNavigationProp<
+type RegisterScreenNavigationProp = StackNavigationProp< //이 또한 새로운 typeScript 타입 
   RootStackParamList,
   'Register'
 >;
 
-type Props = {
+type Props = { //Props라는 새로운 TypeScript 타입을 선언. 현재 컴포넌트가 받아들일 수 있는 props의 형태를 정의
   navigation: RegisterScreenNavigationProp;
 };
 
 const RegisterScreen: React.FC<Props> = ({ navigation }) => {
-   const handleRegistration=(username:string,password:string)=>{
+   const handleRegistration=(id:string,password:string,username:string,nickname:string,sex:string,birthDate:String)=>{
        // Perform registration operation here.
-       console.log(`registering with ${username}`);
+       console.log(`registering with ${id}`);
    }
    
    return(
